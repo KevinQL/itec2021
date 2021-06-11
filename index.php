f2fd9826fe560a372317ff0292ff8f2d6e55938e
kevQL <unajmakev@gmail.com>
2021-06-11 11:54:34 -0500

    programando estructura basica proyecto

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php 
+
+    include_once("views/view_inicio.php");
+?>
---
+++ views/view_inicio.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PAINA DE INICIO DE ITEC</title>
+</head>
+<body>
+    
+    <h1>HOLA MUNDO ITEC!!!</h1>
+
+    <section>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque, vel quas quis ducimus nesciunt porro, blanditiis autem rem assumenda ad quam eum, exercitationem animi! Ipsam corrupti odit similique laudantium assumenda facilis iusto, expedita nulla pariatur dolorum explicabo soluta repudiandae? Nesciunt quod tempore repellat veritatis fuga, quibusdam quisquam obcaecati blanditiis? Sapiente dolore, nemo assumenda, sunt rem magnam vero optio libero eveniet cupiditate dolorum minus quod odio. Alias esse aperiam quidem, doloribus autem assumenda, commodi sunt quis dolor illum dolores quam facilis officiis temporibus impedit asperiores labore pariatur explicabo laborum! Architecto debitis deleniti similique facere numquam aliquid dolores quidem nobis tempore est!
+    </section>
+
+</body>
+</html>
