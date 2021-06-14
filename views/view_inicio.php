00f7f700324d505e6b71cebe8fa7dbcfa1ea412f
kevQL <unajmakev@gmail.com>
2021-06-14 13:10:04 -0500

    ejemplo paleta de colores por utilizar

--- views/view_inicio.php
+++ views/view_inicio.php
@@ -5,10 +5,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PAINA DE INICIO DE ITEC</title>
+
+    <link rel="stylesheet" href="./views/css/main.css">
+
+    <!-- FONTS -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital@1&family=Pangolin&display=swap" rel="stylesheet">
+
+
 </head>
 <body>
     
-    <h1>HOLA MUNDO ITEC!!!</h1>
+    <nav class="nav ">
+        <ul class="menu">
+            <li><a href="#" class="link">INICIO</a></li>
+            <li><a href="#" class="link">CURSOS</a></li>
+            <li><a href="#" class="link">CONTACTANOS</a></li>
+            <li><a href="#" class="link">CERTIFICACION</a>
+                <!-- <ul>
+                    <li><a href="#">SUB MENU 1.1</a></li>
+                </ul> -->
+            </li>
+            <li><a href="#" class="link">SOBRE NOSOTROS</a></li>
+        </ul>
+    </nav>
+
+    <section class="slider ">
+        <div class="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quidem perferendis officiis debitis? Rerum, placeat.
+        </div>
+    </section>
+
+    <section class="cinta ">
+
+    </section>
+
+    <section class="cuerpo ">
+
+    </section>
 
     <section>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque, vel quas quis ducimus nesciunt porro, blanditiis autem rem assumenda ad quam eum, exercitationem animi! Ipsam corrupti odit similique laudantium assumenda facilis iusto, expedita nulla pariatur dolorum explicabo soluta repudiandae? Nesciunt quod tempore repellat veritatis fuga, quibusdam quisquam obcaecati blanditiis? Sapiente dolore, nemo assumenda, sunt rem magnam vero optio libero eveniet cupiditate dolorum minus quod odio. Alias esse aperiam quidem, doloribus autem assumenda, commodi sunt quis dolor illum dolores quam facilis officiis temporibus impedit asperiores labore pariatur explicabo laborum! Architecto debitis deleniti similique facere numquam aliquid dolores quidem nobis tempore est!
