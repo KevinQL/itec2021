ab9f82aa1ad1345e60711cd5b963eb127a2fd6b8
kevQL <unajmakev@gmail.com>
2021-06-15 13:02:23 -0500

    estructura básica v2

--- views/view_inicio.php
+++ views/view_inicio.php
@@ -1,52 +1,190 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PAINA DE INICIO DE ITEC</title>
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="./views/css/main.css">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
-    <!-- FONTS -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital@1&family=Pangolin&display=swap" rel="stylesheet">
+        <!-- FONTS -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital@1&family=Pangolin&display=swap" rel="stylesheet">
+
+        <!-- sweet and google fonts  -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />    
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+        <!-- google fonts    -->
+
+        <!-- font awesome -->
+        <script src="https://kit.fontawesome.com/1c90e8b317.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+        <!-- CSS ME -->
+        <link rel="stylesheet" href="./views/css/main.css">
+
+        <title>INICIO ITEC</title>
+    </head>
+    <body>
+        
+
+        <nav class="k-nav navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand k-logo" href="#">
+                    ITEC
+                    <!-- <img src="./views/assets/logo.jpg" alt="" width="80px"> -->
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link k-link active" aria-current="page" href="#">INICIO <i class="fas fa-home"></i> </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link k-link " href="#">CURSOS <i class="fas fa-mouse"></i> </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link k-link" href="#">EQUIPO <i class="fas fa-users-cog"></i> </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link k-link" href="#">COTACTANOS <i class="fas fa-address-card"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link k-link disabled" href="#" tabindex="-1" aria-disabled="true">CERTIFICADOS <i class="fas fa-certificate"></i> </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link k-link" href="#">QUIENES SOMOS <i class="far fa-address-card"></i> </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link k-link k-btn-classroom" href="#">AULA VIRTUAL <i class="fas fa-layer-group"></i> </a>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <section class="k-presentacion ">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h1>TU FORMACIÓN <br> HACIA EL <br> EXITO</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam sunt culpa, possimus officia dolore deserunt iste quidem dolorum odit quaerat voluptatum aliquid mollitia deleniti, perferendis tempora illum itaque modi aliquam!</p>
+                        <button>ÚNETE, ES GRATIS!</button>
+                    </div>
+                    <div class="col-md-6">
+                        <h1>imagen!!!</h1>
+                        <img src="#" alt="">
+                    </div>
+                </div>
+            </div>
+
+        </section>
 
 
-</head>
-<body>
-    
-    <nav class="nav ">
-        <ul class="menu">
-            <li><a href="#" class="link">INICIO</a></li>
-            <li><a href="#" class="link">CURSOS</a></li>
-            <li><a href="#" class="link">CONTACTANOS</a></li>
-            <li><a href="#" class="link">CERTIFICACION</a>
-                <!-- <ul>
-                    <li><a href="#">SUB MENU 1.1</a></li>
-                </ul> -->
-            </li>
-            <li><a href="#" class="link">SOBRE NOSOTROS</a></li>
-        </ul>
-    </nav>
+        <!-- CINTA DE PRESENTACIÓN DE LA PÁGINA DE ITEC -->
+        <section class="k-cinta py-5">
 
-    <section class="slider ">
-        <div class="text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quidem perferendis officiis debitis? Rerum, placeat.
-        </div>
-    </section>
+            <div class="container ">
+                <h1>ÚNETE Y <br> APRENDE CON <br> LOS MEJORES!!</h1>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta error optio assumenda animi dolor ea autem? Nemo reprehenderit et qui totam officiis quasi, asperiores voluptatum laborum excepturi doloribus magni maxime perferendis, iure cumque laudantium atque delectus est? Voluptatibus corrupti ad tempora
 
-    <section class="cinta ">
+                <div class="row py-4">
+                    <div class="col-md-4">
+                        <h3>Lorem, ipsum.</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis, alias?</p>
+                    </div>
+                    <div class="col-md-4">
+                        <h3>Lorem ipsum dolor sit.</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eum, dolorum fugiat nisi eius exercitationem?</p>
+                    </div>
+                    <div class="col-md-4">
+                        <h3>Lorem, ipsum dolor.</h3>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda suscipit harum, quasi soluta consectetur molestias et expedita provident repellat totam.</p>
+                    </div>
+                </div>
+            </div>
 
-    </section>
+        </section>
 
-    <section class="cuerpo ">
+        <!-- CUERPO 1 -->
+        <section>
+            <div class="container py-5">
+                <h3 class="mb-5">Alcanza tus metas con ITEC</h2>
+                <div class="row text-center">
+                    <div class="col-md-3">
+                        <h4 class="k-titulo-c1">Lorem ipsum dolor sit amet.</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, odit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eligendi ex, quidem provident id consequuntur voluptatum odio excepturi expedita doloribus.</p>
+                    </div>
+                    <div class="col-md-3">
+                        <h4 class="k-titulo-c1">Lorem ipsum dolor sit amet consectetur.</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, odit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eligendi ex, quidem provident id consequuntur voluptatum odio excepturi expedita doloribus.</p>
+                    </div>
+                    <div class="col-md-3">
+                        <h4 class="k-titulo-c1">Lorem ipsum dolor sit.</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, odit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eligendi ex, quidem provident id consequuntur voluptatum odio excepturi expedita doloribus.</p>
+                    </div>
+                    <div class="col-md-3">
+                        <h4 class="k-titulo-c1">Lorem, ipsum dolor.</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, odit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eligendi ex, quidem provident id consequuntur voluptatum odio excepturi expedita doloribus.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
 
-    </section>
+        <!-- SECCION CUERPO 2 -->
+        <section class="k-slider py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-7">
+                        <h3>IMAGEN!!!</h3>
+                    </div>
+                    <div class="col-md-5">
+                        <h4>ENCUENTRA OPCIONES <br> FLEXIBLES Y <br> ASEQUIBLES</h4>
+                        <p class="mt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere similique in et ratione quia recusandae vitae aliquam temporibus ex ipsam libero dolor eos optio, corrupti magni fugiat, adipisci obcaecati cumque id soluta tempora iure commodi! Earum voluptatum consequatur veritatis facere aliquam suscipit quo amet rem neque ipsa. Eius, quod consequatur.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
 
-    <section>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque, vel quas quis ducimus nesciunt porro, blanditiis autem rem assumenda ad quam eum, exercitationem animi! Ipsam corrupti odit similique laudantium assumenda facilis iusto, expedita nulla pariatur dolorum explicabo soluta repudiandae? Nesciunt quod tempore repellat veritatis fuga, quibusdam quisquam obcaecati blanditiis? Sapiente dolore, nemo assumenda, sunt rem magnam vero optio libero eveniet cupiditate dolorum minus quod odio. Alias esse aperiam quidem, doloribus autem assumenda, commodi sunt quis dolor illum dolores quam facilis officiis temporibus impedit asperiores labore pariatur explicabo laborum! Architecto debitis deleniti similique facere numquam aliquid dolores quidem nobis tempore est!
-    </section>
+        <!-- seccion de slider EJMPLO -->
+        <section class="k-slider">
 
-</body>
+            <div class="container k-text">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate quos officiis consequatur obcaecati unde, assumenda deserunt iusto voluptates delectus ipsa.
+            </div>
+
+        </section>
+
+
+        <footer class="container-fluid bg-dark text-center lead text-white py-5">
+
+            <h1>THIS IS THE FOOTER!!</h1>
+
+        </footer>
+
+
+
+
+        <!-- Optional JavaScript; choose one of the two! -->
+
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+        <!-- Option 2: Separate Popper and Bootstrap JS -->
+        <!--
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+        -->
+
+        
+
+    </body>
 </html>
