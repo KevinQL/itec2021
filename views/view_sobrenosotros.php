85e755948671ef178f0d45df51f19d8b0ab30d29
kevQL <unajmakev@gmail.com>
2021-07-06 13:15:35 -0500

    css pagina de quines somos

--- views/view_sobrenosotros.php
+++ views/view_sobrenosotros.php
@@ -16,29 +16,29 @@
         ?>
 
         <!-- SECCIÓN acerca de -->
-        <section class="container">
-            <div class="row">
-                <h4>
-                    ACERCA DE 
-                    "EL TECNOLÓGICO"
-                </h4>
-            </div>
-
-            <div class="row">
-                <div class="col-md-6">
-                    <img src="#" alt="">
+        <section class="k-acercaDe">
+            <div class="container">
+                <div class="h2 k-titulo">
+                    ACERCA DE <span class="k-titulo_tec">"EL TECNOLÓGICO"</span>
                 </div>
-                <div class="col-md-6">
-                    <p>
-                        <h5>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="./views/assets/itec/presentacion.png" alt="" width="100%" >
+                    </div>
+                    <div class="col-md-6 k-acerca_info">
+                        <h4 class="">
                             Hola, somos “EL TECNOLÓGICO”
-                        </h5>                                            
-                        Una institución que brinda educación efectiva, progresiva y completa de libre acceso para todos, con el propósito de impulsar el desarrollo de los profesionales compartiendo herramientas tecnológicas con un contenido enriquecedor a nivel nacional e internacional.
-                    </p>
-                    <p>
-                        <h4>VISIÓN</h4>
-                        Brindar una educación de alta calidad basada en una metodología de capacitación de carácter vivencial con el propósito de formar profesionales éticos con estándares nacionales e internacionales a nivel de Latinoamérica.
-                    </p>
+                        </h4>                                            
+                        <p class="lead text-muted">
+                            Una institución que brinda educación efectiva, progresiva y completa de libre acceso para todos, con el propósito de impulsar el desarrollo de los profesionales compartiendo herramientas tecnológicas con un contenido enriquecedor a nivel nacional e internacional.
+                        </p>
+
+                        <h4 class="">VISIÓN</h4>
+                        <p class="lead text-muted">
+                            Brindar una educación de alta calidad basada en una metodología de capacitación de carácter vivencial con el propósito de formar profesionales éticos con estándares nacionales e internacionales a nivel de Latinoamérica.
+                        </p>
+                    </div>
                 </div>
             </div>
 
@@ -46,111 +46,139 @@
 
 
         <!-- EL TECNOLOGICO EN CIFRAS -->
-        <section class="container">
-            <h4>"EL TECNÓGICO" EN CIFRAS</h4>
-            <div class="row text-center">
-                <div class="col-md-4">
-                    + 65.000 <br>
-                    Estudiantes
-                </div>
-                <div class="col-md-4">
-                    + 60.000 <br>
-                    Estudiantes certificados
-                </div>
-                <div class="col-md-4">
-                    + 20 <br>
-                    Cursos gratuitos cada mes
-                </div>
-                <div class="col-md-12">
-                    + 90% <br>
-                    de egresados satisfechos con la educación recibida.
+        <section class="k-cifras">
+            <div class="container py-5">
+                <h2 class="text-center pt-3 pb-5 k-cifras_title"><span class="title_itec">"EL TECNOLÓGICO"</span> EN CIFRAS</h2>
+                <div class="row text-center">
+                    <div class="col-md-4 lead k-cifras_item">
+                        + 65.000 <br>
+                        Estudiantes
+                    </div>
+                    <div class="col-md-4 lead k-cifras_item">
+                        + 60.000 <br>
+                        Estudiantes certificados
+                    </div>
+                    <div class="col-md-4 lead k-cifras_item">
+                        + 20 <br>
+                        Cursos gratuitos cada mes
+                    </div>
+                    <div class="col-md-12 lead k-cifras_item">
+                        + 90% <br>
+                        de egresados satisfechos con la educación recibida.
+                    </div>
                 </div>
             </div>
         </section>
 
         <!-- POR QUE ESTUDIAR EN EL TECNOLOGICO -->
-        <section class="container">
-            <h4>
-                ¿POR QUÉ ESTUDIAR EN “EL TECNOLÓGICO”?
-            </h4>
-            <p>
-                ** Somos una institución de capacitación con un modelo de educación ejecutiva de calidad que garantiza una formación integral y profesional.
-                <br>
-                ** Nuestros cursos tienen un contenido enriquecedor 100% práctico y didáctico acorde a las necesidades del mercado que permiten desarrollar tus habilidades y capacidades.
-                <br>
-                ** Contamos con un staff de docentes de primer nivel con experiencia académica 
-                <br>
-                ** Acceso libre a más de 20 cursos gratuitos en tiempo real cada mes. (sin pago de matrícula y mensualidad).
-                <br>
-                ** Contamos con un staff de Coordinadores académicos que brindan una atención personalizada de primer nivel.
-                <br>
-                ** Te otorgamos certificado con valor oficial por tu compromiso y desempeño en el curso.
-                <br>
-                ** Diplomados con acreditación universitaria.
+        <section class="k-porqueEstudiar">
+            <div class="container py-5">
+                <h2 class="k-pe_title">
+                    ¿POR QUÉ ESTUDIAR EN <span class="title_itec">“EL TECNOLÓGICO”</span>?
+                </h2>
+                <div class="k-p_lista">
+                    
+                    <ul class="lead">
+                        <li>
+                            <i class="fas fa-history"></i> Somos una institución de capacitación con un modelo de educación ejecutiva de calidad que garantiza una formación integral y profesional.
+                        </li>
+                        <li>
+                            <i class="fas fa-history"></i> Nuestros cursos tienen un contenido enriquecedor 100% práctico y didáctico acorde a las necesidades del mercado que permiten desarrollar tus habilidades y capacidades.
+                        </li>
+                        <li>
+                            <i class="fas fa-history"></i> Contamos con un staff de docentes de primer nivel con experiencia académica 
+                        </li>
+                        <li>
+                            <i class="fas fa-history"></i> Acceso libre a más de 20 cursos gratuitos en tiempo real cada mes. (sin pago de matrícula y mensualidad).
+                        </li>
+                        <li>
+                            <i class="fas fa-history"></i> Contamos con un staff de Coordinadores académicos que brindan una atención personalizada de primer nivel.
+                        </li>
+                        <li>
+                            <i class="fas fa-history"></i> Te otorgamos certificado con valor oficial por tu compromiso y desempeño en el curso.
+                        </li>
+                        <li>
+                            <i class="fas fa-history"></i> Diplomados con acreditación universitaria.
+                        </li>
+                    </ul>
 
-            </p>
+                </div>
+            </div>
         </section>
 
 
         <!-- SECCION metodologia de educaion ejecutiva -->
-        <section class="container">
-            <h4 class="text-center">
-                “EL TECNOLÓGICO”
-            </h4>
-            <h5 class="text-center">
-                Metodología de Educación Ejecutiva
-            </h5>
+        <section class="k-metodologia">
+            <div class="container">
+                <h2 class="text-center pb-5 k-m_title">
+                    <span class="title_itec">“EL TECNOLÓGICO”</span> <br>
+                    Metodología de Educación Ejecutiva
+                </h2>
 
-            <div class="row text-center" >
-                <div class="col-md-3">
-                    <h5>CLASE EN TIEMPO REAL</h5>
-                    <img src="#" alt="itec">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus mollitia voluptatum ipsam assumenda! Dolorem recusandae fuga natus dignissimos quo corrupti!
-                    </p>
-                </div>
-                <div class="col-md-3">
-                    <h5>MATERIALES</h5>
-                    <img src="#" alt="itec">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus mollitia voluptatum ipsam assumenda! Dolorem recusandae fuga natus dignissimos quo corrupti!
-                    </p>
-                </div>
-                <div class="col-md-3">
-                    <h5>VIDEO DE CLASE</h5>
-                    <img src="#" alt="itec">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus mollitia voluptatum ipsam assumenda! Dolorem recusandae fuga natus dignissimos quo corrupti!
-                    </p>
-                </div>
-                <div class="col-md-3">
-                    <h5>MINIVIDEOS DE CLASE</h5>
-                    <img src="#" alt="itec">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus mollitia voluptatum ipsam assumenda! Dolorem recusandae fuga natus dignissimos quo corrupti!
-                    </p>
-                </div>
-                <div class="col-md-3">
-                    <!-- ESPCIO VACIO -->
-                </div>
-                <div class="col-md-3">
-                    <h5>FOROS</h5>   
-                    <img src="#" alt="itec">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus mollitia voluptatum ipsam assumenda! Dolorem recusandae fuga natus dignissimos quo corrupti!
-                    </p>
-                </div>
-                <div class="col-md-3">
-                    <h5>ATENCIÓN PERSONALIZADA</h5>
-                    <img src="#" alt="itec">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus mollitia voluptatum ipsam assumenda! Dolorem recusandae fuga natus dignissimos quo corrupti!
-                    </p>
-                </div>
-                <div class="col-md-3">
-                    <!-- ESPCIO VACIO -->
+                <div class="row text-center" >
+                    <div class="col-md-3">
+                        <div  class="k-card">
+                            <img src="#" alt="itec">
+                            <h5 class="k-card_title">CLASE EN TIEMPO REAL</h5>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus mollitia voluptatum ipsam assumenda! Dolorem recusandae fuga natus dignissimos quo corrupti!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="k-card">
+                            <img src="#" alt="itec">
+                            <h5 class="k-card_title">MATERIALES</h5>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus mollitia voluptatum ipsam assumenda! Dolorem recusandae fuga natus dignissimos quo corrupti!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="k-card">
+                            <img src="#" alt="itec">
+                            <h5 class="k-card_title">VIDEO DE CLASE</h5>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus mollitia voluptatum ipsam assumenda! Dolorem recusandae fuga natus dignissimos quo corrupti!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="k-card">
+                            <img src="#" alt="itec">
+                            <h5 class="k-card_title">MINIVIDEOS DE CLASE</h5>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus mollitia voluptatum ipsam assumenda! Dolorem recusandae fuga natus dignissimos quo corrupti!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <!-- ESPCIO VACIO -->
+                    </div>
+                    <div class="col-md-3">
+                        <div class="k-card">
+                            <img src="#" alt="itec">
+                            <h5 class="k-card_title">FOROS</h5>   
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus mollitia voluptatum ipsam assumenda! Dolorem recusandae fuga natus dignissimos quo corrupti!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="k-card">
+                            <img src="#" alt="itec">
+                            <h5 class="k-card_title">ATENCIÓN PERSONALIZADA</h5>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus mollitia voluptatum ipsam assumenda! Dolorem recusandae fuga natus dignissimos quo corrupti!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <!-- ESPCIO VACIO -->
+                    </div>
                 </div>
             </div>
+
         </section>
 
 
